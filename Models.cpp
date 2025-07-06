6dd262bb2c187d02b5c093f02a436d939cefee7e
OjaswitaPatil <ojaswitapatil.skncoe.comp@gmail.com>
2025-07-06 12:12:02 +0530

    added textuer support fot triangle and pyramid

--- Models.cpp
+++ Models.cpp
@@ -24,62 +24,61 @@ void createTriangle(Model *model)
         1.0f, 0.0f
         };
     
-    // model->numberOfFaces = 1;
+    model->modeltype = TRIANGLE;
 
+    model->numberOfFaces = 1;
 
-    // //allocate memory for vertices in a model struct and fill the vertices 
-    // model->vertices = NULL;
-    // model->verticesSize = sizeof(modelVertices) / sizeof(modelVertices[0]);
-    // model->vertices = (GLfloat*)malloc(sizeof(GLfloat) * model->verticesSize);
-    // for(GLint i = 0; i < model->verticesSize; i++)
-    // {
-    //     model->vertices[i] = modelVertices[i];
-    // }
+    //allocate memory for vertices in a model struct and fill the vertices 
+    model->vertices = NULL;
+    model->verticesSize = sizeof(modelVertices) / sizeof(modelVertices[0]);
+    model->vertices = (GLfloat*)malloc(sizeof(GLfloat) * model->verticesSize);
+    for(GLint i = 0; i < model->verticesSize; i++)
+    {
+        model->vertices[i] = modelVertices[i];
+    }
 
-    // //allocate memory for colors in a model struct and fill the colrs
-    // model->colors = NULL;
-    // model->colorsSize = sizeof(modelColors) / sizeof(modelColors[0]);
-    // model->colors = (GLfloat*)malloc(sizeof(GLfloat) * model->colorsSize);
-    // for(GLint i = 0; i < model->colorsSize; i++)
-    // {
-    //     model->colors[i] = modelColors[i];
-    // }
+    //allocate memory for colors in a model struct and fill the colrs
+    model->colors = NULL;
+    model->colorsSize = sizeof(modelColors) / sizeof(modelColors[0]);
+    model->colors = (GLfloat*)malloc(sizeof(GLfloat) * model->colorsSize);
+    for(GLint i = 0; i < model->colorsSize; i++)
+    {
+        model->colors[i] = modelColors[i];
+    }
 
-    // //allocate memory for texcoords in a model struct and fill the texcords
-    // model->texcoords = NULL;
-    // model->texcoordsSize = sizeof(modelTexCoord) / sizeof(modelTexCoord[0]);
-    // model->texcoords = (GLfloat*)malloc(sizeof(GLfloat) * model->texcoordsSize);
-    // for(GLint i = 0; i < model->texcoordsSize; i++)
-    // {
-    //     model->texcoords[i] = modelTexCoord[i];
-    // }
-    // //allocate memory for textureVariables in a model struct and fill default values in it 
-    // model->textureVariables = NULL;
-    // model->textureVariables = (GLuint*)malloc(sizeof(GLuint) * model->numberOfFaces);
-    // for(GLint i = 0; i < model->numberOfFaces; i++)
-    // {
-    //     model->textureVariables[i] = -1;
-    // }
+    //allocate memory for texcoords in a model struct and fill the texcords
+    model->texcoords = NULL;
+    model->texcoordsSize = sizeof(modelTexCoord) / sizeof(modelTexCoord[0]);
+    model->texcoords = (GLfloat*)malloc(sizeof(GLfloat) * model->texcoordsSize);
+    for(GLint i = 0; i < model->texcoordsSize; i++)
+    {
+        model->texcoords[i] = modelTexCoord[i];
+    }
+    //allocate memory for textureVariables in a model struct and fill default values in it 
+    model->textureVariables = NULL;
+    model->textureVariables = (GLuint*)malloc(sizeof(GLuint) * model->numberOfFaces);
+    for(GLint i = 0; i < model->numberOfFaces; i++)
+    {
+        model->textureVariables[i] = -1;
+    }
 
-    // //normals
-    // model->normals = NULL;
-    // model->normalsSize = 0;
+    //normals
+    model->normals = NULL;
+    model->normalsSize = 0;
 
-    // model->modeltype = TRIANGLE;
+    model->translate.x = 0.0f;
+    model->translate.y = 0.0f;
+    model->translate.z = 0.0f;
 
-    // model->translate.x = 0.0f;
-    // model->translate.y = 0.0f;
-    // model->translate.z = 0.0f;
+    model->scale.x = 1.0f;
+    model->scale.y = 1.0f;
+    model->scale.z = 1.0f;
 
-    // model->scale.x = 1.0f;
-    // model->scale.y = 1.0f;
-    // model->scale.z = 1.0f;
+    model->rotationAngle.x = 0.0f;
+    model->rotationAngle.y = 0.0f;
+    model->rotationAngle.z = 0.0f;
 
-    // model->rotationAngle.x = 0.0f;
-    // model->rotationAngle.y = 0.0f;
-    // model->rotationAngle.z = 0.0f;
-
-    // model->customModelAttributes = NULL;
+    model->customModelAttributes = NULL;
 }
 
 void drawTriangle(Model *model)
@@ -92,35 +91,11 @@ void drawTriangle(Model *model)
     glRotatef(model->rotationAngle.z, 0.0f, 0.0f, 1.0f);
     glScalef(model->scale.x, model->scale.y, model->scale.z);
 
-
-    // glBindTexture(GL_TEXTURE_2D, allLoadedTextureIdentifiers_Array[2]);
-    // glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-
-    // glBegin(GL_TRIANGLES);
-
-    // for(GLint v = 0, c = 0, tc = 0; v < model->verticesSize; v+=3, c+=4, tc+=2)
-    // {
-    //     glTexCoord2f(model->texcoords[tc], model->texcoords[tc+1]);
-    //     //glColor4f(model->colors[c], model->colors[c+1], model->colors[c+2], model->colors[c+3]);
-    //     glVertex3f(model->vertices[v], model->vertices[v+1], model->vertices[v+2]);
-
-    //     // fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
-    //     // fprintf(gpFile, "[DEBUG]Printing primitive details :\n");
-    //     // fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[tc], model->texcoords[tc+1]);
-    //     // fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[v], model->vertices[v+1], model->vertices[v+2]);
-    //     // fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
-    // }
-
-    // glEnd();
-    // glBindTexture(GL_TEXTURE_2D, 0);
-
-
-
     GLint vertexIndex = 0, colorIndex = 0, texCoordIndex = 0;
     for(GLint faceIndex = 0; faceIndex < model->numberOfFaces; faceIndex++)
     {
         BOOL EnableTexture = FALSE;
-        if( (model->textureVariables[faceIndex]) > 0)
+        if( (model->textureVariables[faceIndex]) >= 0)
         {
             EnableTexture = TRUE;
 
@@ -133,6 +108,11 @@ void drawTriangle(Model *model)
         
         glBegin(GL_TRIANGLES);
         {
+            fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+            fprintf(gpFile, "[DEBUG]Printing primitive details :\n");
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex], model->texcoords[texCoordIndex+1]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex], model->vertices[vertexIndex+1], model->vertices[vertexIndex+2]);
+            fflush(gpFile);
             //Vertex1
             if(EnableTexture)
                 glTexCoord2f(model->texcoords[texCoordIndex], model->texcoords[texCoordIndex+1]);
@@ -141,13 +121,21 @@ void drawTriangle(Model *model)
             glVertex3f(model->vertices[vertexIndex], model->vertices[vertexIndex+1], model->vertices[vertexIndex+2]);
 
             //Vertex2
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex+2], model->texcoords[texCoordIndex+3]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex+3], model->vertices[vertexIndex+4], model->vertices[vertexIndex+5]);
+            fflush(gpFile);
             if(EnableTexture)
                 glTexCoord2f(model->texcoords[texCoordIndex+2], model->texcoords[texCoordIndex+3]);
             else
                 glColor4f(model->colors[colorIndex+4], model->colors[colorIndex+5], model->colors[colorIndex+6], model->colors[colorIndex+7]);
             glVertex3f(model->vertices[vertexIndex+3], model->vertices[vertexIndex+4], model->vertices[vertexIndex+5]);
 
+
             //Vertex3
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex+4], model->texcoords[texCoordIndex+5]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex+6], model->vertices[vertexIndex+7], model->vertices[vertexIndex+8]);
+            fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+            fflush(gpFile);
             if(EnableTexture)
                 glTexCoord2f(model->texcoords[texCoordIndex+4], model->texcoords[texCoordIndex+5]);
             else    
@@ -403,25 +391,71 @@ void createPyramid(Model *model)
         1.0f, 1.0f, 0.0f, 1.0f,
         };
 
+     GLfloat modelTexCoord[] = {
+        //FACE 1
+        0.5f, 1.0f,
+        0.0f, 0.0f, 
+        1.0f, 0.0f,
+
+        //FACE 2
+        0.5f, 1.0f,
+        0.0f, 0.0f, 
+        1.0f, 0.0f,
+
+        //FACE 3
+        0.5f, 1.0f,
+        0.0f, 0.0f, 
+        1.0f, 0.0f,
+
+        //FACE 4
+        0.5f, 1.0f,
+        0.0f, 0.0f, 
+        1.0f, 0.0f
+        };
+
+    model->modeltype = PYRAMID;
+
+    model->numberOfFaces = 4;
+
+
+    //allocate memory for vertices in a model struct and fill the vertices 
     model->vertices = NULL;
     model->verticesSize = sizeof(modelVertices) / sizeof(modelVertices[0]);
     model->vertices = (GLfloat*)malloc(sizeof(GLfloat) * model->verticesSize);
-    
     for(GLint i = 0; i < model->verticesSize; i++)
     {
         model->vertices[i] = modelVertices[i];
     }
 
+    //allocate memory for colors in a model struct and fill the colrs
     model->colors = NULL;
     model->colorsSize = sizeof(modelColors) / sizeof(modelColors[0]);
     model->colors = (GLfloat*)malloc(sizeof(GLfloat) * model->colorsSize);
-
     for(GLint i = 0; i < model->colorsSize; i++)
     {
         model->colors[i] = modelColors[i];
     }
 
-    model->modeltype = TRIANGLE;
+    //allocate memory for texcoords in a model struct and fill the texcords
+    model->texcoords = NULL;
+    model->texcoordsSize = sizeof(modelTexCoord) / sizeof(modelTexCoord[0]);
+    model->texcoords = (GLfloat*)malloc(sizeof(GLfloat) * model->texcoordsSize);
+    for(GLint i = 0; i < model->texcoordsSize; i++)
+    {
+        model->texcoords[i] = modelTexCoord[i];
+    }
+    //allocate memory for textureVariables in a model struct and fill default values in it 
+    model->textureVariables = NULL;
+    model->textureVariables = (GLuint*)malloc(sizeof(GLuint) * model->numberOfFaces);
+    for(GLint i = 0; i < model->numberOfFaces; i++)
+    {
+        model->textureVariables[i] = -1;
+    }
+
+
+    //normals
+    model->normals = NULL;
+    model->normalsSize = 0;
 
     model->translate.x = 0.0f;
     model->translate.y = 0.0f;
@@ -436,14 +470,6 @@ void createPyramid(Model *model)
     model->rotationAngle.z = 0.0f;
 
     model->customModelAttributes = NULL;
-
-    //texcoords
-    model->texcoords = NULL;
-    model->texcoordsSize = 0;
-
-    //normals
-    model->normals = NULL;
-    model->normalsSize = 0;
 }
 
 void drawPyramid(Model *model)
@@ -455,15 +481,68 @@ void drawPyramid(Model *model)
     glRotatef(model->rotationAngle.z, 0.0f, 0.0f, 1.0f);
     glScalef(model->scale.x, model->scale.y, model->scale.z);
 
-    glBegin(GL_TRIANGLES);
-
-    for(GLint v = 0, c = 0; v < model->verticesSize; v+=3, c+=4)
+    GLint vertexIndex = 0, colorIndex = 0, texCoordIndex = 0;
+    for(GLint faceIndex = 0; faceIndex < model->numberOfFaces; faceIndex++)
     {
-        glColor4f(model->colors[c], model->colors[c+1], model->colors[c+2], model->colors[c+3]);
-        glVertex3f(model->vertices[v], model->vertices[v+1], model->vertices[v+2]);
-    }
+        BOOL EnableTexture = FALSE;
+        if( (model->textureVariables[faceIndex]) >= 0)
+        {
+            EnableTexture = TRUE;
 
-    glEnd();
+            //bind texture
+            glBindTexture(GL_TEXTURE_2D, allLoadedTextureIdentifiers_Array[model->textureVariables[faceIndex]]);
+
+            //reset color
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        
+        glBegin(GL_TRIANGLES);
+        {
+            fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+            fprintf(gpFile, "[DEBUG]Printing primitive details :\n");
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex], model->texcoords[texCoordIndex+1]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex], model->vertices[vertexIndex+1], model->vertices[vertexIndex+2]);
+            fflush(gpFile);
+            //Vertex1
+            if(EnableTexture)
+                glTexCoord2f(model->texcoords[texCoordIndex], model->texcoords[texCoordIndex+1]);
+            else
+                glColor4f(model->colors[colorIndex], model->colors[colorIndex+1], model->colors[colorIndex+2], model->colors[colorIndex+3]);
+            glVertex3f(model->vertices[vertexIndex], model->vertices[vertexIndex+1], model->vertices[vertexIndex+2]);
+
+            //Vertex2
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex+2], model->texcoords[texCoordIndex+3]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex+3], model->vertices[vertexIndex+4], model->vertices[vertexIndex+5]);
+            fflush(gpFile);
+            if(EnableTexture)
+                glTexCoord2f(model->texcoords[texCoordIndex+2], model->texcoords[texCoordIndex+3]);
+            else
+                glColor4f(model->colors[colorIndex+4], model->colors[colorIndex+5], model->colors[colorIndex+6], model->colors[colorIndex+7]);
+            glVertex3f(model->vertices[vertexIndex+3], model->vertices[vertexIndex+4], model->vertices[vertexIndex+5]);
+
+
+            //Vertex3
+            fprintf(gpFile, "[DEBUG]TexCoord: {%lf, %lf}\n",model->texcoords[texCoordIndex+4], model->texcoords[texCoordIndex+5]);
+            fprintf(gpFile, "[DEBUG]Vertices: {%lf, %lf, %lf}\n",model->vertices[vertexIndex+6], model->vertices[vertexIndex+7], model->vertices[vertexIndex+8]);
+            fprintf(gpFile, "[DEBUG] &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+            fflush(gpFile);
+            if(EnableTexture)
+                glTexCoord2f(model->texcoords[texCoordIndex+4], model->texcoords[texCoordIndex+5]);
+            else    
+                glColor4f(model->colors[colorIndex+8], model->colors[colorIndex+9], model->colors[colorIndex+10], model->colors[colorIndex+11]);
+            glVertex3f(model->vertices[vertexIndex+6], model->vertices[vertexIndex+7], model->vertices[vertexIndex+8]);
+        }
+        glEnd();
+
+        if(EnableTexture)
+            glBindTexture(GL_TEXTURE_2D, 0);
+
+
+        //offset jumps for array
+        vertexIndex += 9;
+        colorIndex += 12,
+        texCoordIndex += 6;
+    }
 
     glPopMatrix();
 }
@@ -602,7 +681,7 @@ void createCube(Model *model)
         model->colors[i] = modelColors[i];
     }
 
-    model->modeltype = TRIANGLE;
+    model->modeltype = CUBE;
 
     model->translate.x = 0.0f;
     model->translate.y = 0.0f;
